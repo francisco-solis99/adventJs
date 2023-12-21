782cd03b3adea8cfd694a6a7462df18a05e4ff88
francisco-solis99 <franciscosoliscss@gmail.com>
2023-12-20 20:39:19 -0600

    test: :white_check_mark: tests for challenge 1

---
+++ adventJs-2023/challenges/challenge1/challenge1.test.js
@@ -0,0 +1,41 @@
+import { expect, test, describe, expectTypeOf } from 'vitest'
+import { findFirstRepeated } from './challenge1'
+
+
+describe('Find the first gift repeated', () => {
+  test('Test 1', () => {
+    expectTypeOf(findFirstRepeated).returns.toEqualTypeOf(-1)
+  })
+
+  test('Test 2', () => {
+    expect(findFirstRepeated([2, 1, 3, 5, 3, 2])).toBe(3)
+  })
+
+  test('Test 3', () => {
+    expect(findFirstRepeated([2, 2])).toBe(2)
+  })
+
+  test('Test 4', () => {
+    expect(findFirstRepeated([2, 4, 3, 5, 1])).toBe(-1)
+  })
+
+  test('Test 5', () => {
+    expect(findFirstRepeated([2, 4, 3, 5, 1])).toBe(-1)
+  })
+
+  test('Test 6', () => {
+    expect(findFirstRepeated([])).toBe(-1)
+  })
+
+  test('Test 7', () => {
+    expect(findFirstRepeated([1, 3, 4, 5, 0, 1, 3, 0, 7])).toBe(1)
+  })
+
+  test('Test 8', () => {
+    expect(findFirstRepeated([10, 20, 30])).toBe(-1)
+  })
+
+  test('Test 9', () => {
+    expect(findFirstRepeated([5, 1, 2, 3, 2, 5, 1])).toBe(2)
+  })
+})
